bbf1a28b0d5c859cc5a9bc96d786d3d51c123a48
farah <qwasmifarah@gmail.com>
2023-08-30 18:47:09 +0300

    autoamtion project

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -2,7 +2,7 @@
 import helpers.WebDriverHelper;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.AfterClass;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
 
